f7608d92949b995afb64d2032c62c8d50eae5122
Derek Lewis <github@lewisd.com>
2015-11-13 17:14:19 -0800

    Using unacceptable-dropwizard.

--- authrite-acctests/src/main/java/com/lewisd/authrite/acctests/framework/driver/SystemDriver.java
+++ authrite-acctests/src/main/java/com/lewisd/authrite/acctests/framework/driver/SystemDriver.java
@@ -4,58 +4,35 @@ import com.lewisd.authrite.AuthriteServiceApplication;
 import com.lewisd.authrite.AuthriteServiceConfiguration;
 import com.lewisd.authrite.acctests.framework.database.H2DatabaseContext;
 import io.dropwizard.testing.ConfigOverride;
-import io.dropwizard.testing.junit.DropwizardAppRule;
 import io.github.unacceptable.database.DatabaseContext;
-import io.github.unacceptable.lazy.Lazily;
+import io.github.unacceptable.dropwizard.context.ApplicationContext;
+import io.github.unacceptable.liquibase.LiquibaseEnhancer;
+import org.junit.Rule;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
-import org.junit.runner.Description;
-import org.junit.runners.model.Statement;
 
 public class SystemDriver {
-    private static final ConfigOverride LOG_THRESHOLD = ConfigOverride.config("logging.level", System.getProperty("app.logging.threshold", "INFO"));
-    private static final String ACCTEST_BASEURI_PROPERTY = "acctest.baseuri";
+    private final DatabaseContext databaseContext = LiquibaseEnhancer.configureContext(new H2DatabaseContext());
 
-    private final DatabaseContext databaseContext = new H2DatabaseContext();
+    private final ApplicationContext<AuthriteServiceConfiguration> appContext =
+            new ApplicationContext<>(
+                    AuthriteServiceApplication.class,
+                    "src/main/resources/authrite-service.yml",
+                    ConfigOverride.config("database.url", databaseContext.databaseUrl()),
+                    ConfigOverride.config("database.user", databaseContext.username()),
+                    ConfigOverride.config("database.password", databaseContext.password()));
 
-    private final DropwizardAppRule<AuthriteServiceConfiguration> appRule = new DropwizardAppRule<>(
-            AuthriteServiceApplication.class,
-            "src/main/resources/authrite-service.yml",
-            LOG_THRESHOLD,
-            ConfigOverride.config("database.url", databaseContext.databaseUrl()),
-            ConfigOverride.config("database.user", databaseContext.username()),
-            ConfigOverride.config("database.password", databaseContext.password()));
-
-    private String baseUrl = null;
+    @Rule
+    private final TestRule rules = rules();
 
     public String baseUrl() {
-        //CHECKSTYLE.OFF: InnerAssignment
-        return baseUrl = Lazily.create(baseUrl, this::buildBaseUrl);
-        //CHECKSTYLE.ON: InnerAssignment
-    }
-
-    private String buildBaseUrl() {
-        final String propertyValue = System.getProperty(ACCTEST_BASEURI_PROPERTY);
-        if (propertyValue != null) {
-            return propertyValue;
-        } else {
-            return String.format("http://localhost:%d/", appRule.getLocalPort());
-        }
+        return appContext.url();
     }
 
     public TestRule rules() {
-        if (System.getProperty(ACCTEST_BASEURI_PROPERTY) == null) {
-            return RuleChain
-                    .outerRule(databaseContext.rules())
-                    .around(appRule);
-        } else {
-            return new TestRule() {
-                @Override
-                public Statement apply(final Statement base, final Description description) {
-                    return base;
-                }
-            };
-        }
+        return RuleChain
+                .outerRule(databaseContext.rules())
+                .around(appContext.rules());
     }
 
     public PublicApiDriver publicApiDriver() {
