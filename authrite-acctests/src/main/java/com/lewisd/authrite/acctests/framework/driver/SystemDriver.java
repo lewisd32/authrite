ee8c3c49d832b83909600d552a20d3e19df8d9be
Derek Lewis <github@lewisd.com>
2015-12-07 19:57:09 -0800

    Experimenting with SystemDriver as a meta-context

--- authrite-acctests/src/main/java/com/lewisd/authrite/acctests/framework/driver/SystemDriver.java
+++ authrite-acctests/src/main/java/com/lewisd/authrite/acctests/framework/driver/SystemDriver.java
@@ -6,13 +6,12 @@ import com.lewisd.authrite.acctests.framework.database.H2DatabaseContext;
 import io.dropwizard.testing.ConfigOverride;
 import io.github.unacceptable.database.DatabaseContext;
 import io.github.unacceptable.dropwizard.context.ApplicationContext;
+import io.github.unacceptable.dropwizard.system.AbstractSystemDriver;
 import io.github.unacceptable.liquibase.LiquibaseEnhancer;
-import org.junit.Rule;
-import org.junit.rules.ExternalResource;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 
-public class SystemDriver {
+public class SystemDriver extends AbstractSystemDriver {
     private final DatabaseContext databaseContext = LiquibaseEnhancer.configureContext(new H2DatabaseContext());
 
     private final ApplicationContext<AuthriteServiceConfiguration> appContext =
@@ -23,27 +22,19 @@ public class SystemDriver {
                     ConfigOverride.config("database.user", databaseContext.username()),
                     ConfigOverride.config("database.password", databaseContext.password()));
 
-    @Rule
-    private final TestRule rules = rules();
-
-    public String baseUrl() {
+    @Override
+    protected String internalAppBaseUrl() {
         return appContext.url();
     }
 
-    public TestRule rules() {
-        if (!appContext.isExternal()) {
-            return RuleChain
-                    .outerRule(databaseContext.rules())
-                    .around(appContext.rules());
-        } else {
-            return new NoOpRule();
-        }
+    @Override
+    protected TestRule internalAppRules() {
+        return RuleChain
+                .outerRule(databaseContext.rules())
+                .around(appContext.rules());
     }
 
     public PublicApiDriver publicApiDriver() {
         return new PublicApiDriver();
-    }
-
-    private class NoOpRule extends ExternalResource {
     }
 }
